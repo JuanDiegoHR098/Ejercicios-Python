d12ca6939d7759f5455131932a745aef2d089db9
JuanDiegoHR098 <juan.hernandez48@udea.edu.co>
2024-02-22 20:30:00 -0500

    Actualizacion para buscar paciente

--- ej_POO.py
+++ ej_POO.py
@@ -1,114 +1,207 @@
+# class Persona:
+#     def __init__(self) -> None:
+#         self.__nombre= ""
+#         self.__cedula= ""
+#         self.__genero= ""
+#         self.__servicio= ""
+
+#     #Getters
+#     def get_nombre(self):
+#         return self.__nombre 
+#     def get_cedula(self):
+#         return self.__cedula
+#     def get_genero(self):
+#         return self.__genero
+#     def get_servicio(self):
+#         return self.__servicio
+    
+#     #Setters
+#     def set_nombre(self,nombre):
+#         self.__nombre= nombre
+#     def set_cedula(self,cedula):
+#         self.__cedula= cedula
+#     def set_genero(self,genero):
+#         self.__genero= genero
+#     def set_servicio(self,servicio):
+#         self.__servicio= servicio
+    
+
+# class Sistema():
+    
+#     def __init__(self) -> None:
+#         self.__lista_paciente=[]
+        
+
+#     def get_listaPacientes(self):
+#         return self.__lista_paciente
+    
+
+    
+#     def get_numeroPacientes(self):
+#         return len(self.__lista_paciente)
+    
+    
+
+
+#     def ingresar_paciente(self,paciente):
+#         #Ingresar ds del paciente
+#         self.__lista_paciente.append(paciente)
+
+            
+#     def verDatosPaciente(self,c):
+#         pacientes_coinciden=[]
+#         # if self.verificarPac(c) == False:
+#         #     return None
+#         for p in self.__lista_paciente:
+#             if c == p.get_cedula():
+#                 return p
+#             if c in p.get_nombre().lower():
+#                 pacientes_coinciden.append(p)
+#             return None
+    
+            
+# sis= Sistema()
+
+# while True:
+    
+    
+#     print("Menu:")
+#     print("¿Que desea hacer?\n1. Ingresar Paciente\n2. Ver datos del paciente\n 3. Ver numero de pacientes\n4. Salir")
+
+#     entrada= int(input("Ingrese una opcion:"))
+
+#     if entrada == 1:
+        
+#         nombre= input("Ingresar nombre del paciente:")
+#         cedula= (input("Ingrese cedula del paciente:"))
+#         genero= input("Genero:")
+#         servicio= input("Ingrese servicio donde se encuentra:")
+
+#         paciente= Persona() 
+#         paciente.set_nombre(nombre)
+#         paciente.set_cedula(cedula)
+#         paciente.set_genero(genero)   
+#         paciente.set_servicio(servicio)
+#         sis.ingresar_paciente(paciente)
+#         print(f"¡El paciente {nombre} se a ingresado satiscatoriamente!")
+        
+#     elif entrada== 2:
+#         validacion= (input("Ingrese el numero de cedula del paciente"))
+#         paciente_encontrado= sis.verDatosPaciente(validacion)
+
+#         if paciente_encontrado == None:
+#             print("Paciente no encontrado")
+#         else:
+            
+#             print("Nombre: ", paciente_encontrado.get_nombre())
+#             print("Genero: ", paciente_encontrado.get_genero())
+#             print("Servicio: ", paciente_encontrado.get_servicio())
+    
+#     elif entrada == 3:
+#         print(f"El numero de pacienes registrado es : {sis.get_numeroPacientes()}")
+            
+        
+
+#     if entrada==4:
+#         break
+
+
 class Persona:
     def __init__(self) -> None:
-        self.__nombre= ""
-        self.__cedula= int
-        self.__genero= ""
-        self.__servicio= ""
+        self.__nombre = ""
+        self.__cedula = ""
+        self.__genero = ""
+        self.__servicio = ""
 
-    #Getters
+    # Getters
     def get_nombre(self):
-        return self.__nombre 
+        return self.__nombre
+
     def get_cedula(self):
         return self.__cedula
+
     def get_genero(self):
         return self.__genero
+
     def get_servicio(self):
         return self.__servicio
-    
-    #Setters
-    def set_nombre(self,nombre):
-        self.__nombre= nombre
-    def set_cedula(self,cedula):
-        self.__cedula= cedula
-    def set_genero(self,genero):
-        self.__genero= genero
-    def set_servicio(self,servicio):
-        self.__servicio= servicio
-    
 
-class Sistema():
-    
+    # Setters
+    def set_nombre(self, nombre):
+        self.__nombre = nombre
+
+    def set_cedula(self, cedula):
+        self.__cedula = cedula
+
+    def set_genero(self, genero):
+        self.__genero = genero
+
+    def set_servicio(self, servicio):
+        self.__servicio = servicio
+
+
+class Sistema:
+
     def __init__(self) -> None:
-        self.__lista_paciente=[]
-        self.__numero_pacientes= len(self.__lista_paciente)
+        self.__lista_paciente = []
 
     def get_listaPacientes(self):
         return self.__lista_paciente
-    
+
     def get_numeroPacientes(self):
-        return self.__numero_pacientes
-    
-    
+        return len(self.__lista_paciente)
 
-
-    def ingresar_paciente(self,paciente):
-        #Ingresar ds del paciente
+    def ingresar_paciente(self, paciente):
         self.__lista_paciente.append(paciente)
 
-    # def ver_datos_paciente(self,c):
-    #     # for i in self.__lista_paciente:
-    #     #     if i.ge in 
-    #         pass
-    
-    # def ver_datos_paciente(self, c):
-    #     for paciente in self.__lista_paciente:
-    #         if paciente == c:
-    #             print(f"Nombre: {paciente.get_nombre()}")
-    #             print(f"Cedula: {paciente.get_cedula()}")
-    #             print(f"Genero: {paciente.get_genero()}")
-    #             print(f"Servicio: {paciente.get_servicio()}")
-    #             return
-    #     print("Paciente no encontrado.")
-    # def verificarPac(self,ced):
-    #     encontrado =  False
-    #     for p in self.__lista_paciente:
-    #         if ced == p.get_cedula():
-    #             encontrado = True
-    #             break
-    #     return encontrado
-            
-    def verDatosPaciente(self,c):
-        # if self.verificarPac(c) == False:
-        #     return None
+    def buscar_paciente(self, clave):
+        pacientes_coinciden = []
+
         for p in self.__lista_paciente:
-            if c == Persona.get_cedula():
-                return p
+            if clave == p.get_cedula() or clave.lower() in p.get_nombre().lower():
+                paciente_info = (
+                    p.get_nombre(), p.get_cedula(), p.get_genero(), p.get_servicio())
+                pacientes_coinciden.append(paciente_info)
+
+        return pacientes_coinciden
+
+sis = Sistema()
 
 while True:
-    
-    sis= Sistema()
     print("Menu:")
-    print("¿Que desea hacer?\n1. Ingresar Paciente\n2. Ver datos del paciente\n 3. Ver numero de pacientes\n4. Salir")
+    print("¿Que desea hacer?\n1. Ingresar Paciente\n2. Ver datos del paciente\n3. Ver numero de pacientes\n4. Salir")
 
-    entrada= int(input("Ingrese una opcion:"))
+    entrada = int(input("Ingrese una opcion:"))
 
     if entrada == 1:
-        
-        nombre= input("Ingresar nombre del paciente:")
-        cedula= int(input("Ingrese cedula del paciente:"))
-        genero= input("Genero:")
-        servicio= input("Ingrese servicio donde se encuentra:")
+        nombre = input("Ingresar nombre del paciente:")
+        cedula = input("Ingrese cedula del paciente:")
+        genero = input("Genero:")
+        servicio = input("Ingrese servicio donde se encuentra:")
 
-        paciente= Persona() 
+        paciente = Persona()
         paciente.set_nombre(nombre)
         paciente.set_cedula(cedula)
-        paciente.set_genero(genero)   
+        paciente.set_genero(genero)
         paciente.set_servicio(servicio)
-        r=sis.ingresar_paciente(paciente)
-        print(f"¡El paciente {nombre} se a ingresado satiscatoriamente!")
-        
-    elif entrada== 2:
-        validacion= int(input("Ingrese el numero de cedula del paciente"))
-        f=sis.verDatosPaciente(validacion)
-        if f == None:
+
+        sis.ingresar_paciente(paciente)
+        print(f"¡El paciente {nombre} se ha ingresado satisfactoriamente!")
+
+    elif entrada == 2:
+        clave_busqueda = input("Ingrese el numero de cedula o nombre del paciente:")
+        pacientes_encontrados = sis.buscar_paciente(clave_busqueda)
+
+        if not pacientes_encontrados:
             print("Paciente no encontrado")
         else:
-            
-            print("Nombre: " + f.get_nombre())
-            
-        
+            for paciente_encontrado in pacientes_encontrados:
+                print("Nombre: {}, Cedula: {}, Genero: {}, Servicio: {}".format(
+                    *paciente_encontrado))
 
-    if entrada==4:
+    elif entrada == 3:
+        print(f"El numero de pacientes registrados es: {sis.get_numeroPacientes()}")
+
+    elif entrada == 4:
         break
-
 
